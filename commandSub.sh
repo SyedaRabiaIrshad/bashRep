77ce0583ea99881921e752cf713c3c797f3d9c66
SyedaRabiaIrshad <syedarabiairshad@gmail.com>
2017-06-20 00:01:46 -0700

    first preoject

---
+++ commandSub.sh
@@ -0,0 +1,5 @@
+DATE=`date`
+echo "Date is $DATE"
+
+UP=`date ; uptime`
+echo "uptime is $UP"
---
+++ exp2.sh
@@ -0,0 +1,5 @@
+. export.sh
+echo "exported text from file export"
+echo TEXT1 = $text1
+echo TEXT2 = $text2
+
